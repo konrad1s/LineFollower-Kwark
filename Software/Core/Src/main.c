7656b4355671340281550bf9bc5ea3d83c35a15c
Konrad Sikora <kontakt@konradsikora.pl>
2024-09-02 22:29:44 +0200

    UART with DMA enabled

--- Software/Core/Src/main.c
+++ Software/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "adc.h"
 #include "crc.h"
+#include "dma.h"
 #include "i2c.h"
 #include "tim.h"
 #include "usart.h"
@@ -95,6 +96,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_ADC1_Init();
   MX_I2C1_Init();
   MX_UART4_Init();
